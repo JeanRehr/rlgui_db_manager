4b7b76e56164f5fa34c66e30a8765e691e8c1ad6
JeanRehr <jean.rehr@gmail.com>
2025-04-15 13:12:36 -0300

    Abstracted away global variables into their  own separate file

---
+++ include/globals.h
@@ -0,0 +1,9 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern int window_width;
+extern int window_height;
+
+void update_window_size(int new_width, int new_height);
+
+#endif // GLOBALS_H
---
+++ src/globals.c
@@ -0,0 +1,9 @@
+#include "globals.h"
+
+int window_width = 1600;
+int window_height = 800;
+
+void update_window_size(int new_width, int new_height) {
+    window_width = new_width;
+    window_height = new_height;
+}
