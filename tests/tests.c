2b20c35fbba913550e6a1be750c86552f7f38b24
JeanRehr <jean.rehr@gmail.com>
2025-04-15 17:32:03 -0300

    test

--- tests/tests.c
+++ tests/tests.c
@@ -1,26 +1,16 @@
+// Including raylib + raygui with #define RAYGUI_IMPLEMENTATION to appease the makefile.
+// As makefile dos not links all src/ files expect main.c for the tests directory
+// It won't see the RAYGUI_IMPLEMENTATION and will complain
+#include <raylib.h>
+#define RAYGUI_IMPLEMENTATION
+#include <raygui.h>
+
 #include <stdio.h>
+
 #include "utilsfn.h"
 
-void int_to_str(int num, char *str)
-{
-	int i = 0;
-
-	while (num > 0) {
-		str[i++] = num % 10 + '0';
-		num /= 10;
-	}
-
-	str[i] = '\0';
-
-	for (int j = 0, k = i - 1; j < k; j++, k--) {
-		char temp = str[j];
-		str[j] = str[k];
-		str[k] = temp;
-	}
-}
-
 int main() {
-    int t = 12345;
+	int t = 12345;
     char str[10];
 
     int_to_str(t, str);
