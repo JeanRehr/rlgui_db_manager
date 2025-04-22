eafc5a6e21b5bd05f28729a7c48ed7f9a1195815
JeanRehr <jean.rehr@gmail.com>
2025-04-22 20:29:02 -0300

    change #pragma once to def guards to keep inline with the rest of the code

--- include/utils_hash.h
+++ include/utils_hash.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef UTILS_HASH_H
+#define UTILS_HASH_H
+
 #include <stddef.h>
 
 // Utility functions for hashing the user database
@@ -7,3 +9,5 @@ void generate_salt(char *salt, size_t len);
 
 // Hashes the password with the given salt into the hash_out buffer
 void hash_password(const char *password, const char *salt, char *hash_out);
+
+#endif // UTILS_HASH_H
