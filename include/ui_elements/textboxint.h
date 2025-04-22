09b076480a1d6422bed91a36a94239390fdc535d
JeanRehr <jean.rehr@gmail.com>
2025-04-22 20:44:59 -0300

    Created a new component textboxint for when there is the need to keep leading zeroes, but input must only integer

---
+++ include/ui_elements/textboxint.h
@@ -0,0 +1,21 @@
+#ifndef TEXTBOXINT_H
+#define TEXTBOXINT_H
+
+#include <external/raylib/raylib.h>
+
+#include "CONSTANTS.h"
+
+// For when there is the need to keep leading zeroes, but input must only integer
+struct textboxint {
+	Rectangle bounds;
+	char input[MAX_INPUT];
+	bool edit_mode;
+	const char *label;
+};
+
+struct textboxint textboxint_init(Rectangle bounds, const char *label);
+
+// In the textboxint_draw there will be a filter to only allow input to be numbers
+void textboxint_draw(struct textboxint *tbi);
+
+#endif // TEXTBOXINT_H
