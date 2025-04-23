68a63f057495cd65bff7babab94614371eb1ddaa
JeanRehr <jean.rehr@gmail.com>
2025-04-23 08:59:23 -0300

    create a username textbox and flags

--- include/ui/ui_login.h
+++ include/ui/ui_login.h
@@ -2,14 +2,22 @@
 #define UI_LOGIN_H
 
 #include "ui_elements/button.h"
+#include "ui_elements/textbox.h"
 #include "ui_elements/textboxsecret.h"
 #include "app_state.h"
 #include "error_handling.h"
+
+enum login_screen_flags {
+	FLAG_LOGIN_DONE = 1 << 0, // 0001: Operation completed
+    FLAG_USERNAME_NOT_EXISTS = 1 << 1,
+    FLAG_WRONG_PASSWD = 1 << 2,
+};
 
 // To manage the state of the login menu screen
 struct ui_login {
 	Rectangle menu_title_bounds;
 
+    struct textbox tb_username;
     struct textboxsecret tbs_password;
     struct button butn_login;
 };
