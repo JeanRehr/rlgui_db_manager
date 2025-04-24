10a9fd0fffeb5c2b9a595ca7df1083f29094899c
JeanRehr <jean.rehr@gmail.com>
2025-04-24 09:41:12 -0300

    add flags and a user struct to be populated during login

--- include/ui/ui_login.h
+++ include/ui/ui_login.h
@@ -4,13 +4,17 @@
 #include "ui_elements/button.h"
 #include "ui_elements/textbox.h"
 #include "ui_elements/textboxsecret.h"
+#include "db/db_manager.h"
 #include "app_state.h"
 #include "error_handling.h"
+#include "user.h"
 
 enum login_screen_flags {
 	FLAG_LOGIN_DONE = 1 << 0, // 0001: Operation completed
-    FLAG_USERNAME_NOT_EXISTS = 1 << 1,
+    FLAG_USER_NOT_EXISTS = 1 << 1,
     FLAG_WRONG_PASSWD = 1 << 2,
+    FLAG_USERNAME_EMPTY = 1 << 3,
+    FLAG_PASSWD_EMPTY = 1 << 4,
 };
 
 // To manage the state of the login menu screen
@@ -20,10 +24,12 @@ struct ui_login {
     struct textbox tb_username;
     struct textboxsecret tbs_password;
     struct button butn_login;
+
+    enum login_screen_flags flag;
 };
 
 void ui_login_init(struct ui_login *ui);
 
-void ui_login_draw(struct ui_login *ui, enum app_state *state, enum error_code *error);
+void ui_login_draw(struct ui_login *ui, enum app_state *state, enum error_code *error, database *user_db, struct user *current_user);
 
 #endif // UI_LOGIN_H
