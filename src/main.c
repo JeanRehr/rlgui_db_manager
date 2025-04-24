a8e90e15edd678ede02fe1e179aba79c71f7ef60
JeanRehr <jean.rehr@gmail.com>
2025-04-24 09:43:54 -0300

    logout button and a status bar saying who is currently logged in

--- src/main.c
+++ src/main.c
@@ -153,6 +153,8 @@ int main()
 
 	// End initializing the register food screen
 
+	button logout_butn = button_init((Rectangle) {window_width - 100, window_height - 60, 0, 30}, "Log Out");
+
 	// Rectangle bounds for the style selector box, it is persistent across all screens
 	Rectangle style_options_bounds = {window_width - 130, 30, 120, 30};
 	Rectangle style_options_label = {style_options_bounds.x, style_options_bounds.y - 25, style_options_bounds.width, 20};
@@ -228,6 +230,13 @@ int main()
 		default:
 			break;
 		}
+
+		if (button_draw_updt(&logout_butn)) {
+			memset(&current_user, 0, sizeof(current_user));
+			app_state = STATE_LOGIN_MENU;
+		}
+
+		GuiStatusBar((Rectangle) {0, window_height - 20, window_width, 20}, TextFormat("Logged: %s", current_user.username));
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
