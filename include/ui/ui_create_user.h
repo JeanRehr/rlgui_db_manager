f890f57c24c13bb7a5e3347f7070a3ef6000ea72
JeanRehr <jean.rehr@gmail.com>
2025-04-26 01:23:06 -0300

    start a create user screen

---
+++ include/ui/ui_create_user.h
@@ -0,0 +1,24 @@
+#ifndef UI_CREATE_USER_H
+#define UI_CREATE_USER_H
+
+#include "ui_elements/button.h"
+#include "db/db_manager.h"
+#include "app_state.h"
+#include "error_handling.h"
+
+enum create_user_screen_flags {
+    FLAG_CREATE_USER_OPERATION_DONE = 1 << 0, // 0001: Submission completed
+};
+
+// To manage the state of the register food screen
+struct ui_create_user {
+    Rectangle menu_title_bounds;
+};
+
+void ui_create_user_init(struct ui_create_user *ui);
+
+void ui_create_user_draw(struct ui_create_user *ui, enum app_state *state, enum error_code *error, database *user_db);
+
+void ui_create_user_updt_pos(struct ui_create_user *ui);
+
+#endif // UI_CREATE_USER_H
