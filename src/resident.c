e4302fd7b7442a996d4e2ac2a165498c63e2cfa3
JeanRehr <jean.rehr@gmail.com>
2025-04-27 21:13:59 -0300

    removed unused resident implementation

--- src/resident.c
+++ src/resident.c
@@ -1,15 +1,1 @@
-#include <string.h>
-
-#include "CONSTANTS.h"
 #include "resident.h"
-
-struct resident
-resident_init(char *name, int age, char *health_status, char *needs, bool medical_assistance, enum gender gender) {
-    struct resident resident = { 0 };
-    strcpy(resident.name, name);
-    resident.age = age;
-    strcpy(resident.health_status, health_status);
-    strcpy(resident.needs, needs);
-    resident.gender = gender;
-    return resident;
-}
