91101044a68b27352b41bf9bbd09b8802206b626
JeanRehr <jean.rehr@gmail.com>
2025-04-28 18:42:44 -0300

    handling back button

--- src/ui/ui_create_user.c
+++ src/ui/ui_create_user.c
@@ -1,7 +1,14 @@
 #include "ui/ui_create_user.h"
 
-void ui_create_user_init(struct ui_create_user *ui) {}
+void ui_create_user_init(struct ui_create_user *ui) {
+    ui->butn_back = button_init((Rectangle) { 20, 20, 0, 30 }, "Back");
+}
 
-void ui_create_user_updt_pos(struct ui_create_user *ui) {}
+void ui_create_user_draw(struct ui_create_user *ui, enum app_state *state, enum error_code *error, database *user_db) {
+    if (button_draw_updt(&ui->butn_back)) {
+        *state = STATE_MAIN_MENU;
+    }
+}
 
-void ui_create_user_draw(struct ui_create_user *ui, enum app_state *state, enum error_code *error, database *user_db) {}
+void ui_create_user_updt_pos(struct ui_create_user *ui) {
+}
