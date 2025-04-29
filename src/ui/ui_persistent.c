1032bce04a86f5c4023387b9908e57c5e1dfe3c4
JeanRehr <jean.rehr@gmail.com>
2025-04-29 12:46:40 -0300

    added the styles

--- src/ui/ui_persistent.c
+++ src/ui/ui_persistent.c
@@ -8,7 +8,7 @@
 void ui_persistent_init(struct ui_persistent *ui) {
     ui->logout_butn = button_init((Rectangle) { window_width - 100, window_height - 60, 0, 30 }, "Log Out");
 
-    ui->style_options_bounds = (Rectangle) { window_width - 130, 30, 120, 30 };
+    ui->style_options_bounds = (Rectangle) { window_width - 110, 30, 100, 30 };
 
     ui->style_options_label =
         (Rectangle) { ui->style_options_bounds.x, ui->style_options_bounds.y - 25, ui->style_options_bounds.width, 20 };
@@ -20,7 +20,7 @@ void ui_persistent_draw(struct ui_persistent *ui, struct user *current_user, enu
     GuiLabel(ui->style_options_label, "Style:");
     GuiComboBox(
         ui->style_options_bounds,
-        "Default;Jungle;Candy;Lavanda;Cyber;Terminal;Ashes;Bluish;Dark;Cherry;Sunny;Enefete;Amber",
+        "Amber;Ashes;Bluish;Candy;Cherry;Cyber;Dark;Enefete;Genesis;Jungle;Lavanda;Light;RLTech;Sunny;Terminal",
         active_style
     );
 
@@ -39,9 +39,10 @@ void ui_persistent_updt_pos(struct ui_persistent *ui) {
     ui->logout_butn.bounds.x = window_width - 100;
     ui->logout_butn.bounds.y = window_height - 60;
 
-    ui->style_options_bounds.x = window_width - 130;
+    ui->style_options_bounds.x = window_width - 110;
 
     ui->style_options_label.x = ui->style_options_bounds.x;
 
     ui->statusbar_bounds.y = window_height - 20;
+    ui->statusbar_bounds.width = window_width;
 }
