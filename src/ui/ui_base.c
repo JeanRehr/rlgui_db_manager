8504e8f4189e52e8b96c87b6b5e7ecc3a7d1a386
JeanRehr <jean.rehr@gmail.com>
2025-05-02 23:43:05 -0300

    corrected function pointer assignment and supression of warnings

--- src/ui/ui_base.c
+++ src/ui/ui_base.c
@@ -2,23 +2,39 @@
 
 #include <stdio.h>
 
-static void ui_default_render(struct ui_base *base) {
+static void ui_default_render(struct ui_base *base, enum app_state *state, enum error_code *error, database *db) {
+    (void)base;
+    (void)state;
+    (void)error;
+    (void)db;
     printf("RENDER NOT IMPLEMENTED FOR THIS SCREEN\n");
 }
 
-static void ui_default_handle_buttons(struct ui_base *base) {
+static void
+ui_default_handle_buttons(struct ui_base *base, enum app_state *state, enum error_code *error, database *db) {
+    (void)base;
+    (void)state;
+    (void)error;
+    (void)db;
     printf("HANDLE BUTTONS NOT IMPLEMENTED FOR THIS SCREEN\n");
 }
 
-static void ui_default_handle_warning_msg(struct ui_base *base) {
+static void
+ui_default_handle_warning_msg(struct ui_base *base, enum app_state *state, enum error_code *error, database *db) {
+    (void)base;
+    (void)state;
+    (void)error;
+    (void)db;
     printf("HANDLE WARNING MSGS NOT IMPLEMENTED FOR THIS SCREEN\n");
 }
 
 static void ui_default_update_positions(struct ui_base *base) {
+    (void)base;
     printf("UPDATE POSITIONS NOT IMPLEMENTED FOR THIS SCREEN\n");
 }
 
 static void ui_default_clear_fields(struct ui_base *base) {
+    (void)base;
     printf("CLEAR FIELDS NOT IMPLEMENTED FOR THIS SCREEN\n");
 }
 
