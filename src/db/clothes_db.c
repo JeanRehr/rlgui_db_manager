3f4f6a9d92852ef214ebb96a605c05a278111ef0
JeanRehr <jean.rehr@gmail.com>
2025-05-24 13:05:09 -0300

    changed some fields location in the table

--- src/db/clothes_db.c
+++ src/db/clothes_db.c
@@ -17,11 +17,11 @@ int clothes_db_create_table(database *db) {
         "CREATE TABLE IF NOT EXISTS Clothes ("
         "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
         "Type TEXT NOT NULL,"                             // e.g. "t-shirt", "pants", "coat"
-        "Size TEXT,"                                      // e.g. "M", "L", "XL", or "42", "kids"
-        "Gender TEXT,"                                    // e.g. "other", "male", "female" NULL permitted for flexibility
+        "Size TEXT,"                                      // e.g. "xs", "s", "m", "l"
+        "Gender TEXT,"                                    // e.g. "other", "male", "female"
         "Color TEXT,"                                     // e.g. "blue", "black", "red"
-        "Quantity INTEGER NOT NULL DEFAULT 0,"            // Items in stock
         "Condition TEXT,"                                 // e.g. "new", "good", "worn", "needs repair"
+        "Quantity INTEGER NOT NULL DEFAULT 0,"            // Items in stock
         "Notes TEXT,"                                     // For arbitrary tracking (e.g., donor, special handling)
         "UNIQUE(Type, Size, Gender, Color, Condition));"; // Prevents accidental duplicate entries of the same clothes type
                                                           // e.g., multiple "t-shirt M male black new"
