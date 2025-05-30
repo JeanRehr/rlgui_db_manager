3cc4ed8986fc1ffc2cb11e2062ab81bfbf8eca65
Jean Rehr <jeanrehr@gmail.com>
2025-05-30 09:59:48 -0300

    changed to an array of characters of max_input size

--- include/entities/medication.h
+++ include/entities/medication.h
@@ -5,22 +5,20 @@
 #ifndef MEDICATION_H
 #define MEDICATION_H
 
-#include <stdbool.h>
-
 #include "global/CONSTANTS.h"
 
-/**
+/** 
  * @struct medication
  *
  * @brief Represents a pieace of medication record in the database
  *
  */
 struct medication {
-    char *name;                     ///< e.g. "Paracetamol 500g"
-    char *generic_name;             ///< e.g. "Paracetamol"
-    char *form;                     ///< e.g. "Tablet", "Syrup", "Injection"
-    char *strength;                 ///< e.g. "500mg", "5mg/ml"
-    char *unit;                     ///< e.g. "Tablet", "ml", "vial"
+    char name[MAX_INPUT];                     ///< e.g. "Paracetamol 500g"
+    char generic_name[MAX_INPUT];             ///< e.g. "Paracetamol"
+    char form[MAX_INPUT];                     ///< e.g. "Tablet", "Syrup", "Injection"
+    char strength[MAX_INPUT];                 ///< e.g. "500mg", "5mg/ml"
+    char unit[MAX_INPUT];                     ///< e.g. "Tablet", "ml", "vial"
     int stock;                      ///< // Current count in inventory
     char expiration_date[DATE_LEN]; ///< Soonest expiration date
     char notes[MAX_INPUT];          ///< General notes if needed
