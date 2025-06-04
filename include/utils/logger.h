43e3ceaf3e6b7c042b420221fabb19a92bbe16bf
Jean Rehr <jeanrehr@gmail.com>
2025-06-04 15:46:24 -0300

    add a logger class

---
+++ include/utils/logger.h
@@ -0,0 +1,76 @@
+/**
+ * @file logger.h
+ * @brief Logger definition to log actions of users
+ */
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdarg.h> // For va_list
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * @struct logger
+ *
+ * @brief Represents the logger structure to open/close file and log username
+ *
+ * @note The username pointer MUST be updated when users log in/out.
+ */
+struct logger {
+    FILE *log_file;       ///< File to open/close to write to
+    const char *username; ///< Pointer to the current user's username (managed externally)
+};
+
+/**
+ * @brief Initializes the logger structure
+ *
+ * Will initialize the log structure with the desired parameters
+ *
+ * @param log Pointer to a logger struct to initialize
+ * @param log_path pointer to the path to write to file (passing null will print to stdout)
+ * @param username Pointer to the name of the username currently logged-in
+ * @return void
+ *
+ */
+void logger_init(struct logger *log, const char *log_path, const char *username);
+
+/**
+ * @brief Logs events that happened
+ *
+ * Will log events that happened to the file in log made by the username set in log struct
+ * printf style with variadic arguments
+ *
+ * @param log Pointer to an initialized logger struct
+ * @param format format printf style (%d for integers, %s for strings, etc.)
+ * @param ... Variadic arguments
+ * @return void
+ *
+ * @code{.c}
+ * #define LOG_PATH "log/application.log"
+ * // Init logger
+ * struct logger log = { 0 };
+ * logger_init(&log, LOG_PATH, &current_user->username);
+ * 
+ * logger_log(&log, "Inserted %d %s.", 10, "Milk");
+ * 
+ * logger_deinit(&log);
+ * @endcode
+ * 
+ * @example output:
+ * 
+ * [2025-06-05 16:30:45] User: alice | Inserted 10 Milk.
+ *
+ */
+void logger_log(struct logger *log, const char *format, ...);
+
+/**
+ * @brief Cleans-up after the logger structure
+ *
+ * @param log Pointer to an initialized logger struct
+ * @return void
+ *
+ */
+void logger_deinit(struct logger *log);
+
+#endif // LOGGER_H
---
+++ src/utils/logger.c
@@ -0,0 +1,66 @@
+/**
+ * @file logger.c
+ * @brief Logger class/struct implementation
+ */
+#include "utils/logger.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "global/CONSTANTS.h"
+
+void logger_init(struct logger *log, const char *log_path, const char *username) {
+    if (!log_path) {
+        log->log_file = stdout;
+        return;
+    }
+    
+    log->log_file = fopen(log_path, "a");
+    
+    if (!log->log_file) {
+        fprintf(stderr, "Failed to open log file: %s\n", log_path);
+        exit(EXIT_FAILURE);
+    }
+
+    log->username = username;
+}
+
+void logger_log(struct logger *log, const char *format, ...) {
+    if (!log) {
+        fprintf(stderr, "Logger not initialized!\n");
+        return;
+    }
+
+    time_t now;
+    time(&now);
+    char timestamp[DATETIME_LEN];
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+    // Print timestamp and user
+    if (!log->username) {
+        log->username = "{Username not set}";
+    }
+
+    fprintf(log->log_file, "[%s] User: %s | ", timestamp, log->username);
+
+    // Variadic arguments printf style
+    va_list args;
+    va_start(args, format);
+    int ret = vfprintf(log->log_file, format, args);
+    va_end(args);
+
+    if (ret < 0) {
+        fprintf(stderr, "Failed to write to log file!");
+        return;
+    }
+
+    fprintf(log->log_file, "\n"); // Add newline
+    fflush(log->log_file); // Force write
+}
+
+void logger_deinit(struct logger *log) {
+    if (log && log->log_file != stdout) {
+        fclose(log->log_file);
+        log->log_file = NULL;
+    }
+}
